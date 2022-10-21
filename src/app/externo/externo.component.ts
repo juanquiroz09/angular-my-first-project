041c048298c9a3690e63618e8a6e5a63d9ccb0ce
--global <juan.quiroz1009@gmail.com>
2022-10-21 16:31:27 -0500

    user_data

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PeticionesService } from '../services/peticiones.service';
 
 @Component({
   selector: 'app-externo',
   templateUrl: './externo.component.html',
-  styleUrls: ['./externo.component.css']
+  styleUrls: ['./externo.component.css'],
+  providers: [PeticionesService],
 })
 export class ExternoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public user: any;
+  public userId: any;
+  constructor(private _peticionesService: PeticionesService) {
+    this.userId = 1;
   }
 
+  ngOnInit(): void {
+    this.cargaUsuario();
+  }
+  cargaUsuario() {
+    this._peticionesService.getUser(this.userId).subscribe(
+      (result) => {
+        this.user = result.data;
+      },
+      (error) => {
+        console.log(<any>error);
+      }
+    );
+  }
 }
--- src/app/services/peticiones.service.ts
+++ src/app/services/peticiones.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from "@angular/core"
-import { HttpClient} from "@angular/common/http"
-import { Observable } from "rxjs"
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
-  export class PeticionesService{
-    public url: string;
-    constructor(public _http: HttpClient){
-      this.url = "https://reqres.in/"
-    }
-
+export class PeticionesService {
+  public url: string;
+  constructor(public _http: HttpClient) {
+    this.url = 'https://reqres.in/';
   }
+  getUser(userId: any): Observable<any> {
+    return this._http.get(this.url + 'api/users/' + userId);
+  }
+}
