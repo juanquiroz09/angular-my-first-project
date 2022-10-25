6d5e0df6c7d736020e90676f262a985f233f461f
--global <juan.quiroz1009@gmail.com>
2022-10-25 09:37:39 -0500

    add_user_bubg_solution

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -6,46 +6,58 @@ import { PeticionesService } from '../services/peticiones.service';
   selector: 'app-externo',
   templateUrl: './externo.component.html',
   styleUrls: ['./externo.component.css'],
-  providers:[PeticionesService]
+  providers: [PeticionesService]
 })
 export class ExternoComponent implements OnInit {
+
   public user: any;
   public userId: any;
   public fecha: any;
 
   public new_user: any;
   public usuario_guardado: any;
-  constructor(private _peticionesService: PeticionesService) {
-    this.userId = 1;
+
+  constructor(
+  	private _peticionesService: PeticionesService
+  ){
+  	this.userId = 1;
     this.new_user = {
-      "name": "",
-      "job": ""
-    };
+                "name": "",
+                "job": ""
+            };
   }
 
-  ngOnInit(): void {
-    this.fecha = Date();
-    this.cargaUsuario();
+  ngOnInit() {
+
+  	this.fecha = new Date(2019, 5, 20);
+
+  	this.cargaUsuario();
   }
-  cargaUsuario() {
-    this.user = false;
-    this._peticionesService.getUser(this.userId).subscribe(
-      (result) => {
-        this.user = result.data;
-      },
-      (error) => {
-        console.log(<any>error);
-      }
-    );
+
+  cargaUsuario(){
+  	this.user = false;
+  	this._peticionesService.getUser(this.userId).subscribe(
+  		result => {
+  			this.user = result.data;
+  		},
+  		error => {
+  			console.log(<any>error);
+  		}
+  	);
   }
-  onSubmit(form: any) {
+
+  onSubmit(form: any){
+
     this._peticionesService.addUser(this.new_user).subscribe(
-      (response) => {
-        console.log(response);
+      response => {
+        this.usuario_guardado = response;
+        form.reset();
       },
-      (error) => {
+      error => {
         console.log(<any>error);
       }
     );
+
   }
+
 }
